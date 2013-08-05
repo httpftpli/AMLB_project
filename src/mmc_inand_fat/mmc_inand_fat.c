78e2be887f44e93ab89e8a89c69eb7f3feb4468a
LFL <httpftpli@hotmail.com>
2013-08-05 14:43:17 +0800

    ccc

--- src/mmc_inand_fat/mmc_inand_fat.c
+++ src/mmc_inand_fat/mmc_inand_fat.c
@@ -19,18 +19,19 @@
 #endif
 #include "gpio_v2.h"
 #include "utf8.h"
-#include "gui.h"
+#include "lib_gui.h"
 #include "pf_lcd.h"
 #include "pf_usbmsc.h"
 #include "pf_platform_cfg.h"
-#include "font.h"
 #include "../include/cpld.h"
 #include "pf_tsc.h"
-#include "event.h"
 #include "pf_uart.h"
 #include "uart_irda_cir.h"
 #include "module.h"
-
+#include "pf_key_touchpad.h"
+#include "lib_gui.h"
+#include "dmtimer.h"
+#include "pf_dmtimer.h"
 /******************************************************************************
 **                      INTERNAL MACRO DEFINITIONS
 *******************************************************************************/
@@ -46,12 +47,12 @@
 
 #define RX8025_ADDR                     0x32
 #define EEPROM_ADDR                     0x50
- 
+
 /* SD card info structure */
 mmcsdCardInfo card0, card1;
 
 /* SD Controller info structure */
-mmcsdCtrlInfo mmcsdctr[2];
+extern mmcsdCtrlInfo mmcsdctr[2];
 
 FATFS fs[2];
 
@@ -70,7 +71,7 @@ unsigned char data[HSMMCSD_DATA_SIZE];
 
 #elif defined(gcc)
 unsigned char data[HSMMCSD_DATA_SIZE]
-                    __attribute__ ((aligned (SOC_CACHELINE_SIZE)))= {0};
+__attribute__((aligned(SOC_CACHELINE_SIZE))) = { 0 };
 #else
 #error "Unsupported Compiler. \r\n"
 #endif
@@ -84,8 +85,8 @@ unsigned char data[HSMMCSD_DATA_SIZE]
 
 volatile unsigned int ack = 0;
 
-static void canrcvhandler(unsigned int index,CAN_FRAME *frame){
-      ack = 1;
+static void canrcvhandler(unsigned int index, CAN_FRAME *frame) {
+   ack = 1;
 }
 
 
@@ -96,96 +97,85 @@ unsigned int fontloaded = 0;
 
 #define FONT_LOAD_ADDR        0x80000000+128*1024*1024
 
-
-void udiskread(){
-   static unsigned int error=0;
-   int r;
-   if((g_usbMscState == USBMSC_DEVICE_READY)&&(fontloaded == 0)&&(error == 0)){
-     r = initFont(L"2:/",FONT_LOAD_ADDR);
-     if (r!=0) {
-        error = 1;
-        return;
-     }
-     fontloaded = 1;   
-   }
+void drawText() {
+   GUI_SetBkColor(C_TRANSPARENT);
+   GUI_SetColor(C_WHITE);
+   GUI_SetFont(fonts[2]);
+   drawString("杭州欢迎你", 30, 30);
+   drawString("杭州はあなたを歓迎する", 30, 60);
+   drawString("Welcome to Hangzhou", 30, 90);
+   drawString("Hangzhou Hoşgeldiniz", 30, 120);
+   drawString("1111111", 30, 150);
+   GUI_SetFont(fonts[3]);
+   drawString("杭州欢迎你", 30, 200);
+   drawString("杭州はあなたを歓迎する", 30, 250);
+   drawString("Welcome to Hangzhou", 30, 300);
+   drawString("Hangzhou Hoşgeldiniz", 30, 350);
+   drawString("1111111", 30, 400);
 }
-
-
-void drawText(){
-   static unsigned int textdrawed = 0;
-   if ((textdrawed == 0)&&(fontloaded == 0)) {
-      textdrawed = 1;
-      Dis_DrawText("杭州欢迎你",30,30,C_White,C_TRANSPARENT);
-      Dis_DrawText("杭州はあなたを歓迎する",30,60,C_White,C_TRANSPARENT);
-      Dis_DrawText("Welcome to Hangzhou",30,90,C_White,C_TRANSPARENT);
-      Dis_DrawText("Hangzhou Hoşgeldiniz",30,120,C_White,C_TRANSPARENT);
-      Dis_DrawText("1111111",30,150,C_White,C_TRANSPARENT);
-   }
-}
-
 
 
 volatile unsigned short cpldbuffer[100];
 extern void hmishow(void);
+extern void probUdisk(void);
 
-int main(void)
-{
+
+
+int main(void) {
    volatile unsigned int initFlg = 1;
-   unsigned int status;
    platformInit();
-   moduleEnable(MODULE_ID_GPIO0);
-   UARTStdioInit();
-   DCANInit(MODULE_ID_DCAN0,CAN_MODE_NORMAL,1000000);
-   LCDRasterStart();	//lcd 
-   initMessageRing();
-   TouchScreenInit();
-   tsEnalbe();
-   TouchCalibrate();
-   hmishow();
-   atomicClear(&touched);
-   unsigned int timertick = TimerTickGet();
-   int num = 0;
-   while (1) {
-      guiExec();
+   static char  buf[512];
+   static char  fmmirrorbuf[512];
+   for(int i=0;i < sizeof buf; i++){
+      buf[i] = i;
    }
-  
-   while(1);
-  
-      static const unsigned char calIcon[] = { 0x01, 0x80, 0x01, 0x80, 0x01, 0x80, 0x01, 0x80,
-      0x01, 0x80, 0x01, 0x80, 0x01, 0x80, 0xFF, 0xFF,
-      0xFF, 0xFF, 0x01, 0x80, 0x01, 0x80, 0x01, 0x80, 0x01,
-      0x80, 0x01, 0x80, 0x01, 0x80, 0x01, 0x80 };
-   Dis_Clear(C_Black);
-   MSG msg;
-   clearMessageRing();
-   while (1) {
-   while(!GetMessage(&msg));
-   Dis_DrawMask(calIcon, msg.xpt - 8,  msg.ypt - 8, 16, 16, C_White, C_TRANSPARENT); 
+
+
+////////////////////////////////////////////
+//dmtimerInitForMatch(MODULE_ID_TIMER3,0xffffffff-0x7ffffff,(0xffffffff-0x7ffffff/2),DMTIMER_FLAG_INTENABLE_MATCH);
+
+//////////////////////////////////////////////////////
+   spiFmromInit(fmmirrorbuf, sizeof fmmirrorbuf);
+   spiFmromRead(0, buf, 1);
+   spiFmromWren();
+   delay(500);
+   spiFmromWrite(0, buf, sizeof buf); 
+   delay(500);
+   spiFmromRead(0, buf, sizeof buf);
+   while (1);
+   moduleEnable(MODULE_ID_GPIO0);
+   DCANInit(MODULE_ID_DCAN0, CAN_MODE_NORMAL, 1000000);
+   LCDRasterStart();    //lcd
+   LCDBackLightON();
+   MMCSDP_CtrlInfoInit(&mmcsdctr[0], MODULE_ID_MMCSD0, 48000000, MMCSD_BUSWIDTH_4BIT, 0,
+                       &card0, NULL, NULL, NULL);
+   MMCSDP_CtrlInit(&mmcsdctr[0]);
+   MMCSDP_CardInit(&mmcsdctr[0], MMCSD_CARD_AUTO);
+
+
+   //MMCSDP_Write(&mmcsdctr[0],buf,128*1024-10,4);
+   //memset(buf,0,sizeof buf);
+   //MMCSDP_Read(&mmcsdctr[0],buf,128*1024-10,4);
+   while (1);
+   TouchCalibrate(0);
+   static FATFS inandfs;
+   f_mount(0, &inandfs);
+   initFont(FONT_LOAD_ADDR);
+   drawText();
 }
 
-   //GPIODirModeSet(SOC_GPIO_0_REGS,30,GPIO_DIR_OUTPUT );
-   //while (1){
-     // GPIOPinWrite(SOC_GPIO_0_REGS,30,1);
-     // delay(10000);
-     // GPIOPinWrite(SOC_GPIO_0_REGS,30,0);
-     // delay(10000);
-   //}
-  // uartInit(MODULE_ID_UART1, 115200,8 ,UART_PARITY_NONE,UART_FRAME_NUM_STB_1
-   //          ,UART_INT_RHR_CTI,8,8);
-   while(1){
-     Dis_Clear(C_Red);
-     delay(10000);
-     Dis_Clear(C_Green);
-     delay(10000);
-     Dis_Clear(C_Blue);
-     delay(10000);
-     Dis_Clear(C_White);
-     delay(10000);
-   }
-    while(1);
+//GPIODirModeSet(SOC_GPIO_0_REGS,30,GPIO_DIR_OUTPUT );
+//while (1){
+// GPIOPinWrite(SOC_GPIO_0_REGS,30,1);
+// delay(10000);
+// GPIOPinWrite(SOC_GPIO_0_REGS,30,0);
+// delay(10000);
+//}
+// uartInit(MODULE_ID_UART1, 115200,8 ,UART_PARITY_NONE,UART_FRAME_NUM_STB_1
+//          ,UART_INT_RHR_CTI,8,8);
 
-   while (1) {
-      usbMscProcess();
+/*
+   while (1) {      
       udiskread();
       drawText();
    }
@@ -195,15 +185,7 @@ int main(void)
    MMCSDP_CtrlInit(& mmcsdctr[0]);
    MMCSDP_CardInit(mmcsdctr,MMCSD_CARD_AUTO);
   
-   DWORD plist[] = {256*1024, -1UL, 0, 0};  /* Divide drive into two partitions */
-   BYTE work[_MAX_SS];
-   unsigned int beginLBA = 64*1024;
-   f_fdiskEx(0,beginLBA, plist, work);  /* Divide physical drive 0 */
-   FATFS fs1,fs2;
-   unsigned char buf[512];
-   f_mount(3, &fs1);
-   f_mkfs(3,0,0);
-
+ 
      
    while(1);
    
@@ -234,21 +216,5 @@ int main(void)
    while(1); 
      
 }
-
-
-
-
-
-
-
-
-/*void mainLoop(0){
-      LCDSwapContex();  
-      Dis_Clear(C_Red);
-      LCDSwapFb(); 
-      
-      LCDSwapContex();
-      Dis_Clear(C_Green);
-      LCDSwapFb();  
-}*/
+*/
 
